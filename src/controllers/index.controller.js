7f912537e6d0f856dd1662e26292845334e71d69
Nuno Martinho <nuno.m.martinho@gmail.com>
2021-02-19 17:34:36 +0000

    Algumas alteracoes

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,28 @@
+/**
+ * @class
+ */
+class IndexController {
+
+    renderIndex(req, res){
+        res.render("index", {titleDocument : "Life Guardian", user: req.user});
+    }
+
+    renderSignUp(req, res){
+        res.render("signup", {titleDocument : "Life Guardian", user: req.user});
+    }
+
+    renderSignIn(req, res){
+        res.render("signin", {titleDocument : "Life Guardian", user: req.user});
+    }
+
+    renderDashboard(req, res){
+      res.render("dashboard", {titleDocument : "Dashboard", user: req.user});
+  }
+
+    renderViewMonitor(req, res){
+      res.render("dashboard-view-monitor", {titleDocument : "Monitores", user: req.user});
+  }
+
+}
+
+module.exports = new IndexController();
---
+++ src/routes/index.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const indexController = require("../controllers/index.controller");
+const auth = require("../utils/auth.utils");
+/**
+ *Roteamento
+*/
+router.get("/", auth.forwardAuthenticated, indexController.renderIndex);
+router.get("/dashboard", auth.isAuthenticated, indexController.renderDashboard);
+router.get("/signup", auth.forwardAuthenticated, indexController.renderSignUp);
+router.get("/signin", auth.forwardAuthenticated, indexController.renderSignIn);
+
+module.exports = router;
