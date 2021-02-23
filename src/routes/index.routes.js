a964970d71e9c4c4e12f3c42c5c286d264d22606
Nuno Martinho <nuno.m.martinho@gmail.com>
2021-02-23 01:10:28 +0000

    tweaks

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,7 +5,7 @@ const auth = require("../utils/auth.utils");
 /**
  *Roteamento
 */
-router.get("/", auth.forwardAuthenticated, indexController.renderIndex);
+router.get("/", indexController.renderIndex);
 router.get("/dashboard", auth.isAuthenticated, indexController.renderDashboard);
 router.get("/signup", auth.forwardAuthenticated, indexController.renderSignUp);
 router.get("/signin", auth.forwardAuthenticated, indexController.renderSignIn);
